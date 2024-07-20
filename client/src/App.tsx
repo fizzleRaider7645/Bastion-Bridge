bb63fb1bbbc38d397713f6da937082c64be5b805
Douglas Uretsky <douglasuretsky@gmail.com>
2024-07-20 13:35:21 -0400

    menucard style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./pages/Dashboard";
 
-// import { Flex, Text, Button } from "@radix-ui/themes";
-
 function App() {
   return (
     <Theme>
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,10 +1,22 @@
 import GithubLoginButton from "../atoms/button/GitHubLogin";
+import { styled } from "@stitches/react";
+import MenuCard from "../atoms/card";
+
+const CenteredContainer = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  backgroundColor: "#f5f5f5",
+});
 
 const Login = () => {
   return (
-    <div>
-      <GithubLoginButton />
-    </div>
+    <CenteredContainer>
+      <MenuCard>
+        <GithubLoginButton />
+      </MenuCard>
+    </CenteredContainer>
   );
 };
 
